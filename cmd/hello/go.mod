8de171b4cff4fda19e2debc79aababf9f1a88015
newbee1939 <hideto3970@gmail.com>
2022-02-20 18:10:13 +0900

    importするライブラリのバージョンを間違えていたので修正した

--- cmd/hello/go.mod
+++ cmd/hello/go.mod
@@ -2,4 +2,11 @@ module main
 
 go 1.16
 
-require fyne.io/fyne/v2 v2.1.2
+require (
+	fyne.io/fyne v1.4.3
+	github.com/go-gl/gl v0.0.0-20210813123233-e4099ee2221f // indirect
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20211024062804-40e447a793be // indirect
+	github.com/godbus/dbus/v5 v5.0.4 // indirect
+	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4 // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+)
--- cmd/hello/main.go
+++ cmd/hello/main.go
@@ -1,17 +1,27 @@
 package main
 
 import (
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/widget"
+	// アプリケーションとウインドウを作成するのに使う
+	"fyne.io/fyne/app"
+	// ウインドウ内に配置する部品を作成するために使う
+	"fyne.io/fyne/widget"
 )
 
 func main() {
+	// 新しいアプリケーションを作成
 	a := app.New()
 
+	// 新しいウインドウを作成
 	w := a.NewWindow("Hello")
+	// ウインドウに表示するコンテンツを設定
 	w.SetContent(
-		widget.NewLabel("Hello Fyne!"),
+		// これで複数のコンテンツを表示できる
+		widget.NewHBox(
+			widget.NewLabel("Hello Fyne!"),
+			widget.NewLabel("This is sample application!"),
+		),
 	)
 
+	// ウインドウを表示し実行する
 	w.ShowAndRun()
 }
